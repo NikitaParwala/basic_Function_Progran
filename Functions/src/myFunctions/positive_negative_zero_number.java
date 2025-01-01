20b49862015a044f9f89089fa4534db70e9461fb
Nisha <Nisha@DESKTOP-HDO2FJ2>
2025-01-01 05:31:02 -0800

    Positive, Negative & Zero

--- Functions/src/myFunctions/positive_negative_zero_number.java
+++ Functions/src/myFunctions/positive_negative_zero_number.java
@@ -1,10 +1,32 @@
 package myFunctions;
 
+import java.util.Scanner;
+
 public class positive_negative_zero_number {
+	static int positive = 0;
+	static int negative = 0;
+	static int zeroCount = 0;
+	
+	public static void calculate(int n) {
+
+		if(n > 0) {
+			positive++;
+		}else if(n == 0){
+			zeroCount++;
+		}else {
+			negative++;
+		}
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		boolean on = true;
+		while(on) {
+			System.out.print("Enter any number : ");
+			int n = sc.nextInt();
+			calculate(n);
+			System.out.println();
+		}	
 	}
 
 }
